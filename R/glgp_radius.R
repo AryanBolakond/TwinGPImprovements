f3ed7341c9a28fea9c9bd64d5240e634843c5e00
AryanBolakond <74111467+AryanBolakond@users.noreply.github.com>
2026-07-14 13:17:22 -0400

    Update glgp_radius.R

--- R/glgp_radius.R
+++ R/glgp_radius.R
@@ -21,17 +21,17 @@ create_gp <- function(xy, x_test, gIndices, theta, predIndices, lNum, leaf_size,
   x_test <- as.matrix(x_test)
   gIndices <- as.integer(gIndices)
   predIndices <- as.integer(predIndices)
-
+  
   dim_ <- ncol(xy) - 1L
   gNum_ <- length(gIndices)
   l_ <- floor(dim_ / 2.0) + 2.0
-
+  
   gParams_ <- rep(0.0, dim_ + 2L)
   yg_ <- xy[gIndices, dim_ + 1L]
-
+  
   searchable_idx <- setdiff(seq_len(nrow(xy)), gIndices)
   searchable_coords <- xy[searchable_idx, seq_len(dim_), drop = FALSE]
-
+  
   env <- new.env(parent = emptyenv())
   env$xy <- xy
   env$x_test <- x_test
@@ -47,6 +47,7 @@ create_gp <- function(xy, x_test, gIndices, theta, predIndices, lNum, leaf_size,
   env$gParams_ <- gParams_
   env$lam_ <- NA_real_
   env$nug_ <- NA_real_
+  env$nug_local_ <- NA_real_
   env$yg_ <- yg_
   env$Rg_ <- matrix(0.0, gNum_, gNum_)
   env$Rl_ <- matrix(0.0, gNum_, gNum_)
@@ -56,7 +57,7 @@ create_gp <- function(xy, x_test, gIndices, theta, predIndices, lNum, leaf_size,
   env$k_upper_ <- NA_integer_
   env$searchable_idx <- searchable_idx
   env$searchable_coords <- searchable_coords
-
+  
   env
 }
 
@@ -92,9 +93,9 @@ compute_local_radius <- function(gp) {
       lMin, k_upper
     ), call. = FALSE)
   }
-
+  
   max_radius <- 0.0
-
+  
   k_min_for <- function(ind, test) {
     if (test) {
       lMin
@@ -104,19 +105,19 @@ compute_local_radius <- function(gp) {
       lMin
     }
   }
-
+  
   radius_for_query <- function(query_row, k_min) {
     get_knn_search(gp, query_row, k_min)$distances[k_min]
   }
-
+  
   for (i in seq_len(nrow(gp$x_test))) {
     max_radius <- max(max_radius, radius_for_query(gp$x_test[i, ], k_min_for(NA, TRUE)))
   }
-
+  
   for (i in seq_len(nrow(gp$xy))) {
     max_radius <- max(max_radius, radius_for_query(gp$xy[i, ], k_min_for(i, FALSE)))
   }
-
+  
   gp$local_radius_ <- max_radius
   gp$k_upper_ <- as.integer(k_upper)
   invisible(gp)
@@ -132,14 +133,14 @@ ensure_local_radius <- function(gp) {
 select_local_neighbors <- function(gp, query_row, test, query_ind) {
   lMin <- gp$lNum_
   knn <- get_knn_search(gp, query_row, gp$k_upper_)
-
+  
   in_radius <- knn$distances <= gp$local_radius_
   local_indices <- knn$indices[in_radius]
-
+  
   if (!test) {
     local_indices <- setdiff(local_indices, query_ind)
   }
-
+  
   if (length(local_indices) < lMin) {
     k_need <- if (!test && query_ind %in% gp$searchable_idx) lMin + 1L else lMin
     knn_fb <- get_knn_search(gp, query_row, k_need)
@@ -154,7 +155,7 @@ select_local_neighbors <- function(gp, query_row, test, query_ind) {
       ), call. = FALSE)
     }
   }
-
+  
   local_indices
 }
 
@@ -169,22 +170,22 @@ predict_point <- function(gp, ind, lam, nugget, test = FALSE, return_sigma = FAL
   yg_ <- gp$yg_
   Ainv_ <- gp$Ainv_
   oneVecG_ <- gp$oneVecG_
-
+  
   query_row <- if (test) {
     gp$x_test[ind, ]
   } else {
     gp$xy[ind, ]
   }
-
+  
   query_ind <- if (test) NA_integer_ else ind
   local_indices <- select_local_neighbors(gp, query_row, test, query_ind)
   lNum_ <- length(local_indices)
   oneVecL_ <- rep(1.0, lNum_)
   oneVecGL_ <- rep(1.0, gNum_ + lNum_)
-
+  
   yl <- gp$xy[local_indices, dim_ + 1L]
   y <- c(yg_, yl)
-
+  
   D <- matrix(0.0, lNum_, lNum_)
   for (u in seq_len(lNum_)) {
     for (v in u:lNum_) {
@@ -206,7 +207,7 @@ predict_point <- function(gp, ind, lam, nugget, test = FALSE, return_sigma = FAL
       }
     }
   }
-
+  
   B <- matrix(0.0, gNum_, lNum_)
   for (u in seq_len(gNum_)) {
     for (v in seq_len(lNum_)) {
@@ -223,23 +224,23 @@ predict_point <- function(gp, ind, lam, nugget, test = FALSE, return_sigma = FAL
       B[u, v] <- (1.0 - lam) * exp(value) + lam * w
     }
   }
-
+  
   CAinv <- t(B) %*% Ainv_
   S <- D - CAinv %*% B
-
+  
   eigS <- eig_sym_asc(S)
   kappa <- max(eigS$values) / min(eigS$values)
   delta <- max(0.0, min(eigS$values) * (kappa - exp(20.0)) / (exp(20.0) - 1.0))
   delta_vec <- oneVecL_ * delta
   Sinv <- eigS$vectors %*% diag(1.0 / (eigS$values + delta_vec), lNum_, lNum_) %*% t(eigS$vectors)
-
+  
   CAinvTSinv <- t(CAinv) %*% Sinv
   Rinv <- matrix(0.0, gNum_ + lNum_, gNum_ + lNum_)
   Rinv[seq_len(gNum_), seq_len(gNum_)] <- Ainv_ + CAinvTSinv %*% CAinv
   Rinv[seq_len(gNum_), gNum_ + seq_len(lNum_)] <- -CAinvTSinv
   Rinv[gNum_ + seq_len(lNum_), seq_len(gNum_)] <- -t(CAinvTSinv)
   Rinv[gNum_ + seq_len(lNum_), gNum_ + seq_len(lNum_)] <- Sinv
-
+  
   row <- query_row[seq_len(dim_)]
   rVec <- numeric(gNum_ + lNum_)
   for (u in seq_len(gNum_ + lNum_)) {
@@ -258,16 +259,16 @@ predict_point <- function(gp, ind, lam, nugget, test = FALSE, return_sigma = FAL
     w <- wg_kernel(dist, theta_, l_)
     rVec[u] <- (1.0 - lam) * exp(value) + lam * w
   }
-
+  
   mu <- (matrix(colSums(Rinv), nrow = 1L) %*% y) / sum(Rinv)
   y_mu <- y - oneVecGL_ * mu[1, 1]
   prediction <- mu + t(rVec) %*% Rinv %*% y_mu
   pred <- prediction[1, 1]
-
+  
   if (!return_sigma) {
     return(pred)
   }
-
+  
   tau2 <- (1.0 / (gNum_ + lNum_)) * t(y_mu) %*% Rinv %*% y_mu
   quad <- (t(rVec) %*% Rinv %*% rVec)[1, 1]
   sigma <- sqrt(tau2[1, 1] * max(1e-7, 1.0 + nugget - quad))
@@ -294,7 +295,7 @@ glgp <- function(
 ) {
   gp <- create_gp(xy, x_test, gIndices, theta, predIndices, lNum, leaf_size, nugget)
   compute_local_radius(gp)
-
+  
   t0 <- proc.time()
   if (is.null(global_params)) {
     estimate_gParams(
@@ -309,29 +310,46 @@ glgp <- function(
   }
   t_gParams <- (proc.time() - t0)["elapsed"]
   cat(sprintf("estimate_gParams : %.3f sec\n", t_gParams))
-
+  
   t0 <- proc.time()
   estimate_sParams(gp)
   t_sParams <- (proc.time() - t0)["elapsed"]
   cat(sprintf("estimate_sParams : %.3f sec\n", t_sParams))
-
+  
   t0 <- proc.time()
   result <- gp_predict(gp)
   if (predict_global) {
     global_result <- gp_predict_global(gp)
+    local_result <- gp_predict_local(gp)
     result <- c(
       result,
       list(
         global_mu = global_result$mu,
-        global_sigma = global_result$sigma
+        global_sigma = global_result$sigma,
+        local_mu = local_result$mu,
+        local_sigma = local_result$sigma
       )
     )
   }
   t_predict <- (proc.time() - t0)["elapsed"]
   cat(sprintf("gp_predict       : %.3f sec\n", t_predict))
-
+  
   cat(sprintf("total            : %.3f sec\n", t_gParams + t_sParams + t_predict))
-
+  
+  if (predict_global) {
+    dim_ <- ncol(as.matrix(xy)) - 1L
+    x_test_mat <- as.matrix(x_test)
+    if (ncol(x_test_mat) >= dim_ + 1L) {
+      y_test <- x_test_mat[, dim_ + 1L]
+      rmse <- glgp_rmse(y_test, result)
+      cat(sprintf("RMSE (hybrid)    : %.6f\n", rmse$rmse))
+      cat(sprintf("RMSE (global)    : %.6f\n", rmse$rmse_global))
+      cat(sprintf("RMSE (local)     : %.6f\n", rmse$rmse_local))
+    } else {
+      cat("predict_global = TRUE but x_test has no response column; skipping RMSE printout.\n")
+    }
+  }
+  
   result
 }
 
